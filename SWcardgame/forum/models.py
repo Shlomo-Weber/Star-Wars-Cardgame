f13662f8fa41a882af3894aa653c3ea1a5bc8874
Shlomo <shlano@vex>
2020-03-16 16:18:36 +0200

    added templates, finished login system

--- SWcardgame/forum/models.py
+++ SWcardgame/forum/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+class Comment(models.Model):
+        user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+        post = models.ForeignKey(Post, on_delete=models.CASCADE)
+        text = models.TextField()
+        created_at = models.DateTimeField(auto_now_add=True)
