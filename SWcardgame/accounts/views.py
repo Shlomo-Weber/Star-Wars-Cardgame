ef8e0e2588af55f438502441b6f2fdcb6edd1dcd
Shlomo <shlano@vex>
2020-03-16 12:48:41 +0200

    added urls, base, navbar and homepage

--- SWcardgame/accounts/views.py
+++ SWcardgame/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .forms import *
+from django.contrib import messages
+from django.contrib.auth import authenticate, logout as dlogout, login as dlogin
 # Create your views here.
+
+# def login(request):
+#
+#     return render(request, )
+
+def home(request):
+
+    return render(request, 'accounts/homepage.html')
--- SWcardgame/sw_api.py
+++ SWcardgame/sw_api.py
@@ -1,15 +1,20 @@
 import requests
 import json
 
+cards = []
+
 def get_characters(number):
-    for number in range(1,30):
-        response = requests.get(f'https://swapi.co/api/people/{number}')
+    for n in range(1,number):
+        response = requests.get(f'https://swapi.co/api/people/{n}')
         print(response)
         info = response.json()
         print(info)
+        char_name = info['name']
+        cards.append(char_name)
+        print(cards)
 
 def get_species(number):
-    for number in range(1,30):
+    for number in range(1,31):
         response = requests.get(f'https://swapi.co/api/species/{number}')
         print(response)
         info = response.json()
@@ -22,4 +27,4 @@ def get_films(number):
         info = response.json()
         print(info)
 
-get_films(8)
+get_characters(6)
