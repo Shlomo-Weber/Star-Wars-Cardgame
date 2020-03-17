1d52c88286d9e4c86d9ffd29b3219777941927ae
Shlomo <shlano@vex>
2020-03-17 16:50:37 +0200

    added models for the cards and totaly botched the api page xD

--- SWcardgame/sw_api.py
+++ SWcardgame/sw_api.py
@@ -1,30 +1,43 @@
 import requests
 import json
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SWcardgame.settings')
+import django
 
-cards=[]
+django.setup()
+from trading_post.models import *
 
 def get_characters(number):
     for n in range(1,number):
-        response = requests.get(f'https://swapi.co/api/people/{n}')
-        print(response)
-        info = response.json()
-        print(info)
-        char_name = info['name']
-        cards.append(char_name)
-        print(cards)
+        char_response = requests.get(f'https://swapi.co/api/people/{n}')
+        print(char_response)
+        char_response=char_response.json()
+        for i in char_response['species'.split('/')]:
+            if i.isdigit():
+                species = Species.objects.get(id=i)
+        print(char_response['name'])
+        if not species:
+            species = Species.objects.get(id=1)
+        card = Card(title=char_response['name'], species = species, )
 
-def get_species(number):
-    for number in range(1,31):
-        response = requests.get(f'https://swapi.co/api/species/{number}')
-        print(response)
-        info = response.json()
-        print(info)
+def get_species():
+    response = requests.get('https://swapi.co/api/species/')
+    for n in range(1, response['count']):
+        response = requests.get(f'https://swapi.co/api/species/{n}')
+        for i in response['species'].split('/'):
+            species = Species.objects.get(id=i)
+            print(response)
+            if not species:
+                species = Species.objects.get(id=1)
+        # species = Species(name = response['name'])
+
+
 
 def get_films(number):
     for number in range(1,8):
-        response = requests.get(f'https://swapi.co/api/films/{number}')
+        response = requests.get(f'https://swapi.co/api/films/{n}')
         print(response)
         info = response.json()
         print(info)
 
-get_characters(6)
+get_characters(4)
--- SWcardgame/trading_post/models.py
+++ SWcardgame/trading_post/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Card(models.Model):
+    title = models.CharField(max_length=60)
+    species = models.ForeignKey('Species', on_delete=models.CASCADE, null=True)
+    rarity = models.ManyToManyField('Film')
+
+
+class Species(models.Model):
+    name = models.CharField(max_length=100)
+
+class Film(models.Model):
+    title = models.CharField(max_length=60)
+
+class Deck(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cards = models.ManyToManyField(Card)
